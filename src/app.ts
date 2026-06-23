3dd0dab6763702f6ca093e42d15441327d8ba134
tanzid109 <tanzid.unkhalil109@gmail.com>
2026-06-23 18:27:26 +0600

    prisma model making

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 
-app.use(cors({
-    origin:config.app_url,
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
+  console.log(user);
   res.send("Hello prisma");
 });
 
